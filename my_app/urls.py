547c05f537d1874c6fa4fb4812d6580899bbbf86
rkyzk <reikoyazaki@yahoo.com>
2023-04-04 13:28:56 +0000

    make more_storeis work

--- my_app/urls.py
+++ my_app/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('about/', views.About.as_view(), name='about'),
+    path('more_stories/', views.PostMoreStories.as_view(), name='more_stories'),
     path('add_story/', views.AddStory.as_view(), name='add_story'),
     path('search/', views.Search.as_view(), name='search'),
     path('<int:id>/', views.MyPage.as_view(), name='my_page'),
@@ -14,7 +15,6 @@ urlpatterns = [
     path('<slug:slug>/bookmark/', views.Bookmark.as_view(), name='bookmark'),
     path('<slug:slug>/update/', views.UpdatePost.as_view(), name='update_post'),
     path('<slug:slug>/delete/', views.DeletePost.as_view(), name='delete_post'),
-    path('more_stories/', views.PostMoreStories.as_view(), name='more_stories'),
     path('delete_comment/<int:id>/', views.DeleteComment.as_view(), name='delete_comment'),
     path('update_comment/<int:id>/', views.UpdateComment.as_view(), name='update_comment')
 ]
